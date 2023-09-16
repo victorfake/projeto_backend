b0ccf46d71bed559863090e7921032c2e4e396b9
victorfake <victorhuebr173@gmail.com>
2023-09-16 10:34:33 -0300

    rotas

---
+++ app/routes/item.routes.js
@@ -0,0 +1,23 @@
+module.exports = app => {
+    const garage = require("../controllers/item.controller.js");
+
+    var router = require("express").Router();
+
+    router.post("/", garage.create);
+
+    router.get("/", garage.findAll);
+
+    router.get("/", garage.findAll);
+
+    router.get("/:id", garage.findOne);
+
+    router.get("/Importd", garage.findAllImportd);
+
+    router.put("/:id", garage.update);
+
+    router.delete("/:id", garage.delete);
+
+    router.delete("/", garage.deleteALL);
+
+    app.use("/api/items", router);
+};
