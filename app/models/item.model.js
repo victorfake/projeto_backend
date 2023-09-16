71449ee758e595ddd10410e6705c738f32b9fb92
victorfake <victorhuebr173@gmail.com>
2023-09-16 10:41:46 -0300

    cego

--- app/models/item.model.js
+++ app/models/item.model.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.INTEGER
         },
         vaule: {
-            type: Sequqlize.DOUBLE
+            type: Sequelize.DOUBLE
         },
         Importd: {
             type: Sequelize.BOOLEAN
